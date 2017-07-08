e420377baa1258a2abee14c20a5bce1b1b6070b8
AshishkrGoyal <15103140ashish@gmail.com>
2017-07-08 10:15:11 +0530

    'temp_conv'

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ const request = require('request');
 const argv = require('yargs').argv;
 var app = express();
 var api_key = "d9422ae85c8faa5ed76bbfd803aef127";
-var city = argv.c||"Noida";
+/*var city = argv.c||"Noida";*/
+var city = process.argv[2]||"Noida";
 var URI =  "http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+api_key;
 /*var url =  "api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}";*/
 request(URI,function (error,response,body) {
@@ -16,8 +17,12 @@ request(URI,function (error,response,body) {
     }
     else
     {
-        var weather = JSON.parse(body);
-        console.log(weather);
+        /*var weather = JSON.parse(body);
+        console.log(weather);*/
         //JSON.stringify(console.log(body));
+        /*console.log(body);*/
+        var weather =  JSON.parse(body);
+        console.log((weather.main.temp)-273);
+        console.log(weather.name, weather.sys.country);
     }
 });
